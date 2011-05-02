ddb4a0e869840eb34ec140dea497017360b9cc67
Paul Linton <paul@Computobox.localdomain>
2011-05-02 00:26:12 -0400

    fixed obstacle

--- nav.obstacle.js
+++ nav.obstacle.js
@@ -2,14 +2,8 @@
 	/* An obstacle in the world. */
 	function Obstacle(type, position, height, width) {
         this.type = type;
-        //for walls, this is the upper left hand corner. for everything else 
-        //this is the center of the object
 		this.position = position;
-        this.height = height;
-        this.width = width;
-        if(type !== "wall") {
-        	this.size = height;
-        }
+        this.size = size;
 	}
 	Obstacle.prototype ={   
         update: function(){
